6d14eebeb4591e3cc1f1834aa029aa7aa0ec9b27
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2026-02-10 21:17:58 +0000

    refactor: Use Radix UI Slot utility for Surface

--- src/components/elements/surface.tsx
+++ src/components/elements/surface.tsx
@@ -1,48 +1,39 @@
+import * as SlotUtility from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import { twMerge } from "tailwind-merge";
 
-const cvaSurface = cva(
-  "border-primary-200 overflow-hidden rounded-md border bg-white p-4",
+const surfaceStyles = cva(
+  "border-primary-200 overflow-hidden rounded-xl border bg-white p-4",
   {
     variants: {
       shadow: {
         true: "shadow",
-        false: "",
-      },
-      interactive: {
-        true: "hover:bg-primary-100 active:bg-primary-200 cursor-pointer hover:shadow",
-        false: "",
+        false: null,
       },
     },
     defaultVariants: {
       shadow: false,
-      interactive: false,
     },
   },
 );
 
-export const Surface = (
-  p: React.HTMLAttributes<HTMLElement> &
-    VariantProps<typeof cvaSurface> & {
-      as?: React.ElementType;
-      shadow?: boolean;
-      interactive?: boolean;
-    },
-) => {
-  const Tag = p.as ?? "div";
+type SurfaceProps = React.ComponentProps<"div"> &
+  VariantProps<typeof surfaceStyles> & {
+    asChild?: boolean;
+  };
+
+export const Surface = ({ shadow, className, ...p }: SurfaceProps) => {
+  const Element = p.asChild ? SlotUtility.Root : "div";
 
   return (
-    <Tag
-      {...{ ...p, shadow: undefined, interactive: undefined }}
+    <Element
+      {...p}
       className={twMerge(
-        cvaSurface({
-          shadow: p.shadow,
-          interactive: p.interactive,
-          className: p.className,
+        surfaceStyles({
+          shadow: shadow,
+          className: className,
         }),
       )}
-    >
-      {p.children}
-    </Tag>
+    />
   );
 };
