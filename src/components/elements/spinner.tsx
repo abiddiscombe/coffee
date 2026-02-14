9767cc01d315d669874bfd9e42375a1822505915
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2026-02-14 22:29:21 +0000

    feat: Add Spinner "base" color variant

--- src/components/elements/spinner.tsx
+++ src/components/elements/spinner.tsx
@@ -1,29 +1,36 @@
 import { cva, type VariantProps } from "class-variance-authority";
 
-const spinnerStyles = cva(
-  "rounded-full animate-spin border border-accent-200 dark:border-accent-900 border-r-accent dark:border-r-accent-400",
-  {
-    variants: {
-      size: {
-        sm: "size-3 border-2",
-        md: "size-4 border-2",
-        lg: "size-8 border-4",
-      },
+const spinnerStyles = cva("rounded-full animate-spin border", {
+  variants: {
+    size: {
+      sm: "size-3 border-2",
+      md: "size-4 border-2",
+      lg: "size-8 border-4",
     },
-    defaultVariants: {
-      size: "md",
+    color: {
+      base: "border-base-200 border-r-base",
+      accent: "border-accent-200 border-r-accent",
     },
   },
-);
+  defaultVariants: {
+    size: "md",
+    color: "accent",
+  },
+});
 
 type SpinnerProps = React.ComponentProps<"div"> &
   VariantProps<typeof spinnerStyles>;
 
-export const Spinner = ({ size, className, ...passthrough }: SpinnerProps) => {
+export const Spinner = ({
+  size,
+  color,
+  className,
+  ...passthrough
+}: SpinnerProps) => {
   return (
     <div
       role="status"
-      className={spinnerStyles({ size, className })}
+      className={spinnerStyles({ size, color, className })}
       {...passthrough}
     />
   );
